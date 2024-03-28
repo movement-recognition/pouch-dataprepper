d9450137cb4eedc4b7229934b8f8eaa294684158
Leon Schnieber <olaginos.buero@outlook.de>
2024-03-28 14:54:44 +0100

    added har-parser to dataprepper-script

--- src/dataprepper.py
+++ src/dataprepper.py
@@ -63,6 +63,8 @@ def load_data_to_csv(grafanaconfigfile, tagfilter, outputfile):
     annotation_list = annotation_req.json()
 
     data = []
+    if tagfilter == "":
+        print("no tagFilter supplied, using all tagged data existing")
     for a in annotation_list:
         filter_match = False
         for subfil in a["tags"]:
@@ -98,6 +100,132 @@ def load_data_to_csv(grafanaconfigfile, tagfilter, outputfile):
 
     data_df.to_csv(outputfile)
 
+@cli.command()
+@click.option("--inputFile", default="data/rawExport.csv", help="Filename read the data from in 'raw' column format")
+@click.option("--outputFile", default="data/harExport.csv", help="Filename to output the data to. Outputs in 'few-hundret-column'-format")
+def raw_csv_to_har_format(inputfile, outputfile):
+    import numpy as np
+    import scipy as scp
+    import pandas as pd
+    #import spectrum as spec
+    import math
+    import time
+    from matplotlib import pyplot as plt
+
+    data_df = pd.read_csv(inputfile)
+    if "ticks" in data_df:
+        data_df["ticks"] = data_df["ticks"] / 1000 + time.time()
+        data_df["time"] = pd.to_datetime(data_df['ticks'],unit='s')
+        del data_df["ticks"]
+    else:
+        data_df["time"] = pd.to_datetime(data_df["time"])
+
+    ## delete all unnamed columns (e.g. the "0"-column in the exports)
+    data_df = data_df.loc[:, ~data_df.columns.str.contains('^Unnamed')]
+    
+    ## set index. important!
+    data_df = data_df.set_index("time")
+
+    #### add the "magnitude columns combining xyz/abc"
+    data_df["xyzH"] = np.sqrt(data_df["xH"] ** 2 + data_df["yH"] ** 2 + data_df["zH"] ** 2)
+    data_df["xyzL"] = np.sqrt(data_df["xL"] ** 2 + data_df["yL"] ** 2 + data_df["zL"] ** 2)
+    data_df["xyzG"] = np.sqrt(data_df["xG"] ** 2 + data_df["yG"] ** 2 + data_df["zG"] ** 2)
+    data_df["abcG"] = np.sqrt(data_df["aG"] ** 2 + data_df["bG"] ** 2 + data_df["cG"] ** 2)
+
+    #### compensate the data-aquisition-jitter (by upsampling and interpolation)
+    # Alternative: Use nearest-neighbour or spline-interpolation instead of linear one?
+    # method=linear = ignores the index and treats them as equally spaced. not suitable for usecase, use method=time!
+    data_df = data_df.resample("0.001s").mean().interpolate(method='time')
+    # optional: downsample again to e.g. 50Hz used in HAR-Dataset?
+
+    #### remove noise and split into "body" and "gravitation"-branches
+    # define butterworth filter
+    def bworth_filter(data, f_sample, order, f_corner, btype="low"):
+        cutoff = f_corner / (0.5 * f_sample)
+        b, a = scp.signal.butter(order, cutoff, btype=btype, analog=False)
+
+        return scp.signal.filtfilt(b, a, data)
+
+    # apply filter to all columns
+    body_df = data_df.apply(lambda col: bworth_filter(col, 1000, 3, 20))
+
+    gravity_df = data_df.apply(lambda col: bworth_filter(col, 1000, 3, 0.3))
+
+    #baseline_df = pd.merge_asof(body_df, gravity_df, left_index=True, right_index=True, direction="nearest", suffixes=("_body", "_gravity"))
+    baseline_df = pd.merge(body_df, gravity_df, left_index=True, right_index=True, how="outer", suffixes=("_body", "_gravity"))
+
+    # derivate "acceleration to jerk" 
+    derivative_df = baseline_df.apply(lambda col: np.gradient(col, edge_order=2))
+
+    intermediate_df = pd.merge(baseline_df, derivative_df, left_index=True, right_index=True, how="outer", suffixes=("_accel", "_jerk"))
+
+    #### Sampling/Batching for statistical feature generation
+    ## sample by time duration
+    TIME_DURATION = "0.5s"
+    intermediate_df["time_tmp"] = intermediate_df.index
+    intermediate_df['window_label'] = intermediate_df["time_tmp"].apply(lambda ts: ts.floor(TIME_DURATION))
+
+    ## sample by count
+        #BATCH_SIZE = 128
+        #intermediate_df['window_label'] = np.arange(len(intermediate_df)) // BATCH_SIZE
+
+    #### process the groups one after another
+    output_data = []
+
+    grouped_df = intermediate_df.groupby('window_label')
+    for window_label, group in grouped_df:
+        del group["time_tmp"]
+        del group["window_label"]
+        timestamps = list(group.index.map(lambda _: _.timestamp()))
+        
+        # add basic metrics
+        v_mean = group.mean().add_suffix("_Tmean")
+        v_std = group.std().add_suffix("_Tstd")
+        v_mad = group.apply(lambda col: scp.stats.median_abs_deviation(col.values)).add_suffix("_Tmad")
+        v_min = group.min().add_suffix("_Tmin")
+        v_max = group.max().add_suffix("_Tmax")
+        v_sma = group.apply(lambda col: scp.integrate.simpson(y=np.abs(col.values), x=timestamps)).add_suffix("_Tsma")
+        v_iqr = group.apply(lambda col: np.subtract(*np.percentile(col.values, [75, 25]))).add_suffix("_Tiqr")
+        v_entropy = group.apply(lambda col: scp.stats.entropy(col.values)).add_suffix("_Tentropy")
+        v_energy = group.apply(lambda col: np.average(np.power(col.values, 2))).add_suffix("_Tenergy")
+
+
+        # add AR-coefficients https://pyspectrum.readthedocs.io/en/latest/ref_param.html#spectrum.burg.arburg
+        #arburg_order = 4
+        #for n in range(arburg_order):
+        #column_list.append()
+        #arburg = group.apply(lambda col: spec.arburg(np.fabs(col.values), 4)[0]).add_suffix(f"_Tarburg_coeff")
+            #column_list.append(group.apply(lambda col: spec.arburg(col.values, arburg_order)[0].real[n]).add_suffix(f"_Tar_coeff_{n}"))
+
+        # add Frequency Space information
+        #def apply_fft(col, band_count=8):
+        v_fft_list = []
+        fft_band_count = 8
+        for col_name in group:
+            col = group[col_name]
+            bands_data = scp.fft.fft(col.values)
+            energy_spectrum = np.abs(bands_data[:len(bands_data)//2])**2
+            band_width = math.ceil(len(bands_data) / (fft_band_count * 2))
+            bands_energy = {}
+            for i in range(fft_band_count):
+                if i == 0: # ignore the DC-Part of FFT
+                    s_idx = 1
+                else:
+                    s_idx = i * band_width
+                e_idx = min(len(energy_spectrum), (i+1)*band_width)
+                subband_energy = np.sum(energy_spectrum[s_idx:e_idx])
+                bands_energy[col.name + f"_FenergyBand_{i}"] = subband_energy
+            v_fft_list.append(pd.Series(bands_energy))
+        v_fft = pd.concat(v_fft_list)
+
+        # add to the output table
+        combi = pd.concat([v_mean, v_std, v_mad, v_min, v_max, v_sma, v_iqr, v_entropy, v_energy, v_fft], axis=0)
+        
+        output_data.append(combi)
+
+    final_data = pd.DataFrame(output_data)
+    print("SHAPE", final_data.shape)
+    final_data.to_csv(outputfile)
 
 if __name__ == "__main__":
     cli()
