28b4dc82748b9aa9687ee2403e24bce033af740a
Leon Schnieber <olaginos.buero@outlook.de>
2024-03-28 13:51:06 +0100

    added dataprepper-CLI

--- src/dataprepper.py
+++ src/dataprepper.py
@@ -7,34 +7,90 @@ __status__ = "Development"
 
 import json
 import requests
+import datetime
+import click
 import pandas as pd
 
-with open("config.json", "r") as f:
-    config = json.load(f)
+@click.group()
+def cli():
+    pass
 
-auth_header = {"Authorization": f"Bearer {config['grafana_token']}"}
 
-annotation_url = f"{config['grafana_base_url']}/api/annotations?from=1406556756303&to=1806560356303&limit=100&matchAny=false&dashboardUID={config['grafana_dashboard_uid']}"
-annotation_req = requests.get(annotation_url, headers=auth_header)
+@cli.command()
+@click.option("--grafanaConfigfile", default="config.json", help="path to the config-file containing the grafana config-data.")
+@click.option("--tagFilter", default="", help="filter the annotations by their tag")
+def list_annotations(grafanaconfigfile, tagfilter):  
+    with open("config.json", "r") as f:
+        config = json.load(f)
 
-annotation_list = annotation_req.json()
+    auth_header = {"Authorization": f"Bearer {config['grafana_token']}"}
+    annotation_url = f"{config['grafana_base_url']}/api/annotations?from=0&to=180656035630300&limit=10000&matchAny=false&dashboardUID={config['grafana_dashboard_uid']}"
+    annotation_req = requests.get(annotation_url, headers=auth_header)
 
-for a in annotation_list:
-    print(a["time"], a["timeEnd"], a["text"], a["tags"])
-    startTime = a["time"]
-    endTime = a["timeEnd"]
-    data_query = f'SELECT "x","y","z" FROM "autogen"."schnieboard_meas" WHERE time >= {startTime}ms and time <= {endTime}ms;'
-    data_url = f"{config['grafana_base_url']}/api/datasources/proxy/uid/{config['grafana_datasource_uid']}/query?db=master&q={requests.utils.quote(data_query)}"
-    data_req = requests.get(data_url, headers=auth_header)
+    annotation_list = annotation_req.json()
 
-    data_list = data_req.json()
-    data_subset = data_list["results"][0]["series"][0]
-    data_cols = data_subset["columns"]
+    print("     StartTime          EndTime           Description            Tags")
+    for a in annotation_list:
+        filter_match = False
+        for subfil in a["tags"]:
+            if subfil in tagfilter.strip().split(","):
+                filter_match = True
+                break
+            
+        if filter_match or tagfilter == "":
+            selstr = "\033[92m[x]\033[0m"
+        else:
+            selstr = "\033[91m[ ]\033[0m"
+
+        date_string_format = '%y-%m-%dT%H:%M:%S'
+        start_time_str = datetime.datetime.utcfromtimestamp(a["time"] / 1000).strftime(date_string_format)
+        end_time_str = datetime.datetime.utcfromtimestamp(a["timeEnd"]/1000).strftime(date_string_format)
+        print(selstr, str(start_time_str).rjust(18), str(end_time_str).rjust(18), a["text"].ljust(22), a["tags"])
+
+
+@cli.command()
+@click.option("--grafanaConfigfile", default="config.json", help="path to the config-file containing the grafana config-data.")
+@click.option("--tagFilter", default="", help="filter the annotations by their tag")
+@click.option("--outputFile", default="data/rawExport.csv", help="Filename to output the data to")
+def load_data_to_csv(grafanaconfigfile, tagfilter, outputfile):
+    with open("config.json", "r") as f:
+        config = json.load(f)
+
+    auth_header = {"Authorization": f"Bearer {config['grafana_token']}"}
+    annotation_url = f"{config['grafana_base_url']}/api/annotations?from=0&to=180656035630300&limit=10000&matchAny=false&dashboardUID={config['grafana_dashboard_uid']}"
+    annotation_req = requests.get(annotation_url, headers=auth_header)
+
+    annotation_list = annotation_req.json()
+
     data = []
-    for row in data_subset["values"]:
-        data.append(dict(zip(data_cols, row)))
+    for a in annotation_list:
+        filter_match = False
+        for subfil in a["tags"]:
+            if subfil in tagfilter.strip().split(","):
+                filter_match = True
+                break
 
+        if filter_match or tagfilter == "":
+
+            startTime = a["time"]
+            endTime = a["timeEnd"]
+            data_query = f'SELECT aG, bG, cG, xG, yG, zG, xH, yH, zH, xL, yL, zL FROM "autogen"."schnieboard_meas" WHERE time >= {startTime}ms and time <= {endTime}ms;'
+            data_url = f"{config['grafana_base_url']}/api/datasources/proxy/uid/{config['grafana_datasource_uid']}/query?db=master&q={requests.utils.quote(data_query)}"
+            data_req = requests.get(data_url, headers=auth_header)
+
+            data_list = data_req.json()
+            if "series" in data_list["results"][0]:
+                data_subset = data_list["results"][0]["series"][0]
+                data_cols = data_subset["columns"]
+                
+                for row in data_subset["values"]:
+                    data.append(dict(zip(data_cols, row)))
+            
 
     data_df = pd.DataFrame(data)
 
-    data_df.to_csv(f'data/raw_{a["text"].replace(" ", "_")}_.txt')
+    data_df.to_csv(outputfile)
+
+
+if __name__ == "__main__":
+    cli()
