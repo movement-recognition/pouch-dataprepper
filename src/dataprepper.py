fa32c947ee29c66bfae05244a629f65979d18cd0
Leon Schnieber <olaginos.buero@outlook.de>
2024-08-08 00:48:27 +0200

    improved export script

--- src/dataprepper.py
+++ src/dataprepper.py
@@ -221,11 +221,12 @@ def load_data_to_csv(grafanaconfigfile, tagfilter, outputfile, mergethreshold):
 @cli.command()
 @click.option("--inputFile", default="data/rawExport.csv", help="Filename read the data from in 'raw' column format")
 @click.option("--outputFile", default="data/harExport.csv", help="Filename to output the data to. Outputs in 'few-hundret-column'-format")
-@click.option("--oversamplingFreq", default=1000, type=float, help="Oversampling frequency for data-dejittering. Defaults to 1kHz")
+@click.option("--oversamplingFreq", default=1000, type=float, help="Oversampling frequency for data-dejittering. Defaults to 1kHz. If set to zero or negative, oversampling is deactivated.")
 @click.option("--chunkSize", default=500, type=int, help="Chunk size used for grouping and statistical analysis. defaults to 500ms.")
 @click.option("--chunkOverlap", default=0, type=float, help="Overlap between two chunks/windows used for statistical analysis")
-@click.option("--usedDataTracks", default="xyzH,xyzL,xyzG,abcG", help="comma seperated list of data tracks to be used")
-def raw_csv_to_har_format(inputfile, outputfile, oversamplingfreq, chunksize, chunkoverlap, useddatatracks):
+@click.option("--usedDataTracks", default="xyzH,xyzL,xyzG,abcG", help="comma seperated list of data tracks to be used. possible values: xyzH,xyzL,xyzG,abcG")
+@click.option("--usedStatFilters", default="mean,std,mad,min,max,sma,iqr,entropy,energy,energy_band", help="comma seperated list of statistical features to be calculated. see doc/performance.md for further elaboration. possible values: mean,std,mad,min,max,sma,iqr,entropy,energy,energy_band")
+def raw_csv_to_har_format(inputfile, outputfile, oversamplingfreq, chunksize, chunkoverlap, useddatatracks, usedstatfilters):
     import numpy as np
     import scipy as scp
     import pandas as pd
@@ -233,6 +234,11 @@ def raw_csv_to_har_format(inputfile, outputfile, oversamplingfreq, chunksize, ch
     import math
     import time
     from matplotlib import pyplot as plt
+
+    # preprocessing all parameters
+
+    useddatatracks = [_.strip() for _ in useddatatracks.strip().split(",")]
+    usedstatfilters = [_.strip() for _ in usedstatfilters.strip().split(",")]
 
     print("1/ reading input file")
     data_df = pd.read_csv(inputfile)
@@ -281,9 +287,14 @@ def raw_csv_to_har_format(inputfile, outputfile, oversamplingfreq, chunksize, ch
     #### compensate the data-aquisition-jitter (by upsampling and interpolation)
     # Alternative: Use nearest-neighbour or spline-interpolation instead of linear one?
     # method=linear = ignores the index and treats them as equally spaced. not suitable for usecase, use method=time!
-    print(f"3/ resampling dataset to virtual aquisition rate of {oversamplingfreq} Hz")
-    oversampling_timedelta = 1 / oversamplingfreq
-    data_df = data_df.resample(f"{oversampling_timedelta}s").mean().interpolate(method='time')
+    if oversamplingfreq > 0:
+        print(f"3/ resampling dataset to virtual aquisition rate of {oversamplingfreq} Hz")
+        oversampling_timedelta = 1 / oversamplingfreq
+        data_df = data_df.resample(f"{oversampling_timedelta}s").mean().interpolate(method='time')
+    else: # do not resample (reduce RAM consumption), estimate the sampling frequency by using the first two elements
+        oversampling_timedelta = (data_df.index[1] - data_df.index[0]).total_seconds()
+        oversamplingfreq = 1.0 / oversampling_timedelta
+        print(f"3/ NOT resampling dataset. instead assuming sample rate of {oversamplingfreq} Hz")
     # optional: downsample again to e.g. 50Hz used in HAR-Dataset?
 
     #### remove noise and split into "body" and "gravitation"-branches
@@ -334,16 +345,26 @@ def raw_csv_to_har_format(inputfile, outputfile, oversamplingfreq, chunksize, ch
         group = intermediate_df[group_start_idx:group_end_idx]
         timestamps = list(group.index.map(lambda _: _.timestamp()))
         
+        statistic_features = []
         # add basic metrics
-        v_mean = group.mean().add_suffix("_Tmean")
-        v_std = group.std().add_suffix("_Tstd")
-        v_mad = group.apply(lambda col: scp.stats.median_abs_deviation(col.values)).add_suffix("_Tmad")
-        v_min = group.min().add_suffix("_Tmin")
-        v_max = group.max().add_suffix("_Tmax")
-        v_sma = group.apply(lambda col: scp.integrate.simpson(y=np.abs(col.values), x=timestamps)).add_suffix("_Tsma")
-        v_iqr = group.apply(lambda col: np.subtract(*np.percentile(col.values, [75, 25]))).add_suffix("_Tiqr")
-        v_entropy = group.apply(lambda col: scp.stats.entropy(col.values)).add_suffix("_Tentropy")
-        v_energy = group.apply(lambda col: np.average(np.power(col.values, 2))).add_suffix("_Tenergy")
+        if "mean" in usedstatfilters:
+            statistic_features.append(group.mean().add_suffix("_Tmean"))
+        if "std" in usedstatfilters:
+            statistic_features.append(group.std().add_suffix("_Tstd"))
+        if "mad" in usedstatfilters:
+            statistic_features.append(group.apply(lambda col: scp.stats.median_abs_deviation(col.values)).add_suffix("_Tmad"))
+        if "min" in usedstatfilters:
+            statistic_features.append(group.min().add_suffix("_Tmin"))
+        if "max" in usedstatfilters:
+            statistic_features.append(group.max().add_suffix("_Tmax"))
+        if "sma" in usedstatfilters:
+            statistic_features.append(group.apply(lambda col: scp.integrate.simpson(y=np.abs(col.values), x=timestamps)).add_suffix("_Tsma"))
+        if "iqr" in usedstatfilters:
+            statistic_features.append(group.apply(lambda col: np.subtract(*np.percentile(col.values, [75, 25]))).add_suffix("_Tiqr"))
+        if "entropy" in usedstatfilters:
+            statistic_features.append(group.apply(lambda col: scp.stats.entropy(col.values)).add_suffix("_Tentropy"))
+        if "energy" in usedstatfilters:
+            statistic_features.append(group.apply(lambda col: np.average(np.power(col.values, 2))).add_suffix("_Tenergy"))
 
 
         # add AR-coefficients https://pyspectrum.readthedocs.io/en/latest/ref_param.html#spectrum.burg.arburg
@@ -355,27 +376,28 @@ def raw_csv_to_har_format(inputfile, outputfile, oversamplingfreq, chunksize, ch
 
         # add Frequency Space information
         #def apply_fft(col, band_count=8):
-        v_fft_list = []
-        fft_band_count = 8
-        for col_name in group:
-            col = group[col_name]
-            bands_data = scp.fft.fft(col.values)
-            energy_spectrum = np.abs(bands_data[:len(bands_data)//2])**2
-            band_width = math.ceil(len(bands_data) / (fft_band_count * 2))
-            bands_energy = {}
-            for i in range(fft_band_count):
-                if i == 0: # ignore the DC-Part of FFT
-                    s_idx = 1
-                else:
-                    s_idx = i * band_width
-                e_idx = min(len(energy_spectrum), (i+1)*band_width)
-                subband_energy = np.sum(energy_spectrum[s_idx:e_idx])
-                bands_energy[col.name + f"_FenergyBand_{i}"] = subband_energy
-            v_fft_list.append(pd.Series(bands_energy))
-        v_fft = pd.concat(v_fft_list)
+        if "energy_band" in usedstatfilters:
+            v_fft_list = []
+            fft_band_count = 8
+            for col_name in group:
+                col = group[col_name]
+                bands_data = scp.fft.fft(col.values)
+                energy_spectrum = np.abs(bands_data[:len(bands_data)//2])**2
+                band_width = math.ceil(len(bands_data) / (fft_band_count * 2))
+                bands_energy = {}
+                for i in range(fft_band_count):
+                    if i == 0: # ignore the DC-Part of FFT
+                        s_idx = 1
+                    else:
+                        s_idx = i * band_width
+                    e_idx = min(len(energy_spectrum), (i+1)*band_width)
+                    subband_energy = np.sum(energy_spectrum[s_idx:e_idx])
+                    bands_energy[col.name + f"_FenergyBand_{i}"] = subband_energy
+                v_fft_list.append(pd.Series(bands_energy))
+            statistic_features.append(pd.concat(v_fft_list))
 
         # add to the output table
-        combi = pd.concat([v_mean, v_std, v_mad, v_min, v_max, v_sma, v_iqr, v_entropy, v_energy, v_fft], axis=0)
+        combi = pd.concat(statistic_features, axis=0)
         
         output_data.append(combi)
         if group_id % (window_count/20) == 0:
--- src/export.sh
+++ src/export.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-outXFile="/tmp/conclusion_X.har"
-outyFile="/tmp/conclusion_y.har"
+outXFile="data/conclusion_X.csv"
+outyFile="data/conclusion_y.csv"
+
+tempDir="/tmp/dataprepper"
 
 CHUNK_SIZE=500
 CHUNK_OVERLAP=0.6
-OVERSAMPLING_FREQ=1000
+OVERSAMPLING_FREQ=0 # disable oversampling because of heavy RAM usage
 
-python3 dataprepper.py load-data-to-csv --tagFilter="dr-out_evt;lf-dw_evt" --mergeThreshold=100 --outputFile="data/export_down.csv"
-python3 dataprepper.py load-data-to-csv --tagFilter="dr-un_evt;lf-up_evt" --mergeThreshold=100 --outputFile="data/export_up.csv"
-python3 dataprepper.py load-data-to-csv --tagFilter="idl_mov" --mergeThreshold=100 --outputFile="data/export_idle.csv"
-python3 dataprepper.py load-data-to-csv --tagFilter="dec_mov;acc_mov;crus_mov;turn_mov" --mergeThreshold=100 --outputFile="data/export_movement.csv" 
+mkdir -p $tempDir
+
+python3 dataprepper.py load-data-to-csv --tagFilter="dr-out_evt;lf-dw_evt" --mergeThreshold=100 --outputFile="$tempDir/export_down.csv"
+python3 dataprepper.py load-data-to-csv --tagFilter="dr-un_evt;lf-up_evt" --mergeThreshold=100 --outputFile="$tempDir/export_up.csv"
+python3 dataprepper.py load-data-to-csv --tagFilter="idl_mov" --mergeThreshold=100 --outputFile="$tempDir/export_idle.csv"
+python3 dataprepper.py load-data-to-csv --tagFilter="dec_mov;acc_mov;crus_mov;turn_mov" --mergeThreshold=100 --outputFile="$tempDir/export_movement.csv" 
 
 
 # run the csv-to-har process in parallel to speed up the process
 pids=()
 commands=(
-    "python3 dataprepper.py raw-csv-to-har-format --inputFile=data/export_down.csv --outputFile=data/export_down.har --chunkSize=$CHUNK_SIZE --chunkOverlap=$CHUNK_OVERLAP --oversamplingFreq=$OVERSAMPLING_FREQ"
-    "python3 dataprepper.py raw-csv-to-har-format --inputFile=data/export_up.csv --outputFile=data/export_up.har --chunkSize=$CHUNK_SIZE --chunkOverlap=$CHUNK_OVERLAP --oversamplingFreq=$OVERSAMPLING_FREQ"
-    "python3 dataprepper.py raw-csv-to-har-format --inputFile=data/export_idle.csv --outputFile=data/export_idle.har --chunkSize=$CHUNK_SIZE --chunkOverlap=$CHUNK_OVERLAP --oversamplingFreq=$OVERSAMPLING_FREQ"
-    "python3 dataprepper.py raw-csv-to-har-format --inputFile=data/export_movement.csv --outputFile=data/export_movement.har --chunkSize=$CHUNK_SIZE --chunkOverlap=$CHUNK_OVERLAP --oversamplingFreq=$OVERSAMPLING_FREQ"
+    "python3 dataprepper.py raw-csv-to-har-format --inputFile=$tempDir/export_down.csv --outputFile=$tempDir/export_down.har.csv \
+    --chunkSize=$CHUNK_SIZE --chunkOverlap=$CHUNK_OVERLAP --oversamplingFreq=$OVERSAMPLING_FREQ \
+    --usedDataTracks=xyzH,xyzL,xyzG,abcG --usedStatFilters=mean,std,mad,min,max,sma,iqr,energy,energy_band" # entropy
+
+    "python3 dataprepper.py raw-csv-to-har-format --inputFile=$tempDir/export_up.csv --outputFile=$tempDir/export_up.har.csv \
+    --chunkSize=$CHUNK_SIZE --chunkOverlap=$CHUNK_OVERLAP --oversamplingFreq=$OVERSAMPLING_FREQ \
+    --usedDataTracks=xyzH,xyzL,xyzG,abcG --usedStatFilters=mean,std,mad,min,max,sma,iqr,energy,energy_band" # entropy
+
+    "python3 dataprepper.py raw-csv-to-har-format --inputFile=$tempDir/export_idle.csv --outputFile=$tempDir/export_idle.har.csv \
+    --chunkSize=$CHUNK_SIZE --chunkOverlap=$CHUNK_OVERLAP --oversamplingFreq=$OVERSAMPLING_FREQ \
+    --usedDataTracks=xyzH,xyzL,xyzG,abcG --usedStatFilters=mean,std,mad,min,max,sma,iqr,energy,energy_band" # entropy
+
+    "python3 dataprepper.py raw-csv-to-har-format --inputFile=$tempDir/export_movement.csv --outputFile=$tempDir/export_movement.har.csv \
+    --chunkSize=$CHUNK_SIZE --chunkOverlap=$CHUNK_OVERLAP --oversamplingFreq=$OVERSAMPLING_FREQ \
+    --usedDataTracks=xyzH,xyzL,xyzG,abcG --usedStatFilters=mean,std,mad,min,max,sma,iqr,energy,energy_band" # entropy
 )
 
 for cmd in "${commands[@]}"; do
@@ -35,19 +50,17 @@ echo "" > $outXFile
 echo "" > $outyFile
 
 loop_index=0
-for export in data/*.har; do
+for export in $tempDir/*.har.csv; do
     # category name
-    fn=$(basename $export .har | sed -e 's/export_//')
+    fn=$(basename $export .har.csv | sed -e 's/export_//')
     line_count=$(wc -l < "$export")
 
     if [[ "$loop_index" == 0 ]]; then # skip the first row in all files except the first one (only one header is needed)
-        echo "first"
         # dump to X-File
         cat $export >> $outXFile
         # dump label to y-File
         yes $fn | head -n $line_count >> $outyFile
     else
-        echo "other"
         ((line_count--)) # ignore the header line
         cat $export | tail -n+2 >> $outXFile
         # dump label to y-File
