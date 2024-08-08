e9186a12222c06582b858897b4ac4ec61578069a
Leon Schnieber <olaginos.buero@outlook.de>
2024-08-08 13:03:08 +0200

    added statistics-view

--- src/data_sanitizer.py
+++ src/data_sanitizer.py
@@ -13,6 +13,8 @@ import pandas as pd
 
 from util import filter_function, fetch_annotations, tag_list_to_struct, sanitize_tag_struct
 
+date_string_format = '%y-%m-%dT%H:%M:%S'
+
 @click.group()
 def cli():
     pass
@@ -53,7 +55,6 @@ def sanitize_tags(grafanaconfigfile, outputfile):
     for a in annotation_list:
         tag_struct = tag_list_to_struct(a["tags"])
 
-        date_string_format = '%y-%m-%dT%H:%M:%S'
         duration = a["timeEnd"] - a["time"]
         clickable_url = f"https://smartpouch.foobar.rocks/d/be583447-7432-4ed2-b95d-bf6261b94c27/label-dashboard-don-t-delete?orgId=1&from={a['time']-duration*0.2}&to={a['timeEnd']+duration*0.2}"
         start_time_str = datetime.datetime.fromtimestamp(a["time"] / 1000, tz=datetime.timezone.utc).strftime(date_string_format)
@@ -76,6 +77,65 @@ def sanitize_tags(grafanaconfigfile, outputfile):
     output_str += "</body></html>"
     with open(outputfile, "w") as f:
         f.write(output_str)
+
+
+
+@cli.command()
+@click.option("--grafanaConfigfile", default="config.json", help="path to the config-file containing the grafana config-data.")
+@click.option("--outputFile", default="statistics.html", help="file to output the statistics-view")
+def calc_statistics(grafanaconfigfile, outputfile):
+    import io
+    import base64
+    from matplotlib import pyplot as plt
+
+    raw_struct = {}
+    annotation_list = fetch_annotations(grafanaconfigfile)
+    try:
+        generation_time_string = datetime.datetime.now(datetime.UTC).isoformat() + " (UTC)"
+    except BaseException:
+        generation_time_string = datetime.datetime.utcnow().isoformat() + " (UTC)"
+
+    # generate statistics-struct
+    for a in annotation_list:
+        tag_struct = tag_list_to_struct(a["tags"])
+        start_time = datetime.datetime.fromtimestamp(a["time"] / 1000, tz=datetime.timezone.utc)
+        end_time = datetime.datetime.fromtimestamp(a["timeEnd"] / 1000, tz=datetime.timezone.utc)
+        delta = end_time - start_time
+
+        for t in tag_struct:
+            if t not in raw_struct:
+                raw_struct[t] = {}
+            for t_i in tag_struct[t]:
+                if t_i not in raw_struct[t]:
+                    raw_struct[t][t_i] = 0
+                raw_struct[t][t_i] += delta.total_seconds()
+    
+    # generate report
+    output_str = f"""
+    <html><meta charset="utf-8">
+    <head>
+        <title>Tagging Statistics</title>
+    </head>
+    <body>
+        <h1>Tagging Statistics <small>generated at {generation_time_string}</small></h1>
+    """
+
+    for t in raw_struct:
+        plt.clf()
+        plt.title(f"plot {t}")
+        plt.pie(raw_struct[t].values(), labels=raw_struct[t].keys(), autopct=lambda x: f"{x:2.1f} min")
+
+        file_handler = io.BytesIO()
+        plt.savefig(file_handler, format='png')
+        file_handler.seek(0)
+        file_base64 = base64.b64encode(file_handler.read()).decode()
+        
+        output_str += f"<img src='data:image/png;base64,{file_base64}' />"
+
+    output_str += "</body></html>"
+    with open(outputfile, "w") as f:
+        f.write(output_str)
+
 
 if __name__ == "__main__":
     cli()
